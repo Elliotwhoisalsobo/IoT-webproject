938d06349a10493a1aa7e5b394a251f0c41ab015
Elliot <146078541+Elliotwhoisalsobo@users.noreply.github.com>
2025-11-11 13:17:54 +0100

    Implemented working POST & soft deletion for device api endpoint

--- routes/device.js
+++ routes/device.js
@@ -20,49 +20,65 @@ router.get('/', async (req, res) => { // async = db enabled
 // [POST] Devices 
 // return id (id kan ook null zijn, niet gelukt )
 // -------------------------
-router.post('/', async (req, res) => {
-  const deviceName = req.body.name;
+router.post('/', async (req, res) => { 
+    const { device_name, device_purpose, device_ip, status } = req.body;
 
-  const checkDeviceExists = await prisma.device.findMany({
-    where: {
-      name: deviceName
-    }
-  })
+    const device = await prisma.device.create({
+        data: {
+            device_name,
+            device_purpose,
+            device_ip,
+            status
+        }
+    });
 
-  if (checkDeviceExists.length > 0) {
-    res.json({
-      "status": "device already in database"
-    })
-  } else {
+    console.log(device_name, device_purpose, device_ip, status);
+    res.send(device);
+});
 
-  const newDevice = await prisma.device.create({
-  
-    data: { 
-      name: deviceName
-    }
-  })
-    }
-    res.json(newDevice);
-})
+
 
 
 // -------------------------
 // [PUT] Devices 
 // return boolean (true or false )
 // -------------------------
-router.put('/:id', (req, res) => {
-  // @todo: link to database
-  // req.body -> om data uit een put te halen
-  res.send("Updated device");
-})
+// router.put('/:id', (req, res) => {
+//   // @todo: link to database
+//   // req.body -> om data uit een put te halen
+//   res.send("Updated device");
+// })
+
+router.put('/:id', async (req, res) => {
+
+    const updatedDevice = await prisma.device.update({
+      where: {deviceid: + req.params.id},
+      data: req.body
+    });
+    res.json(updatedDevice);
+
+});
 
 // -------------------------
 // [DELETE] Devices 
 // return boolean (true or false )
 // -------------------------
-router.delete('/:id', (req, res) => {
-  // @todo: link to database
-  res.send("Deleted device");
-})
+// SOFT DELETION
+router.delete('/:id', async (req, res) => {
+
+    const id = parseInt(req.params.id);
+    // Update "isdeleted" with current timestamp instead of deleting
+    await prisma.$executeRawUnsafe(
+      `UPDATE device SET isdeleted = CURRENT_TIMESTAMP WHERE deviceid = ${id}`
+    );
+    res.json({ success: true, message: `Record ${id} timestamped & marked as deleted` });
+});
+
+
+// HARD DELETION
+// router.delete('/:id', (req, res) => {
+//   // @todo: link to database
+//   res.send("Deleted device");
+// })
 
 module.exports = router;

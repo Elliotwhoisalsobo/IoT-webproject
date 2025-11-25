9b1ffc44b4a506539286f8ce1e34bae048561830
Elliot <146078541+Elliotwhoisalsobo@users.noreply.github.com>
2025-11-25 13:20:39 +0100

    Added initial raspi pi integration

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const cors = require("cors");
 const app = express()
 
+
+
 app.use(cors());
 //app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json()); // my app may send and receive json data !!!
@@ -23,4 +25,10 @@ app.use('/sensor', sensorRouter);
 
 console.log("API IS UP AND RUNNING!!!")
 
-app.listen(3000)
+app.listen(3000, '0.0.0.0') // Laptop IP at dorm --> 10.10.0.188
+
+ // RASPBERRY PI
+app.post("/api/pi-data", (req, res) => {
+    console.log("Data from Pi:", req.body);
+    res.status(200).json({ message: "received" });
+});
